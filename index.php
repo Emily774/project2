4cedf12c3440cb7238466425a6b53d6f909c24fd
Louise de Mattos <72737315+Emily774@users.noreply.github.com>
2021-04-12 12:48:35 +0100

    Updated db name to Timelord2

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-$db = new PDO('mysql:host=db; dbname=Timelord', 'root', "password");
+$db = new PDO('mysql:host=db; dbname=Timelord2', 'root', "password");
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-$query = $db->prepare('SELECT * FROM forgallifrey');
+$query = $db->prepare('SELECT * FROM `forgallifrey`');
 $query->execute();
 $forgallifreyArray = $query->fetchAll();
 
@@ -51,7 +51,7 @@ foreach ($forgallifreyArray as $timelord){
                 <video muted loop controls>
                     <source src='Pexels Videos 3867.mp4' type='video/mp4'>
                 </video>
-                <img src='34DD0CEF-7DEB-4FAC-A0D5-F3E1E85C7082.jpg' class='images' alt='The Doctor (David Tennant) stares into the camera in a blue suit and against an orange background.'>
+                <img src='The_Doctor.jpg' class='images' alt='The Doctor (David Tennant) stares into the camera in a blue suit and against an orange background.'>
                 <p><button>Add The Doctor</button></p>";
            echo "<p class='dataBox'>";
        }
@@ -60,7 +60,7 @@ foreach ($forgallifreyArray as $timelord){
                 <video muted loop controls>
                     <source src='mastervid.mov' type='video/mp4'>
                 </video>
-                <img class='images' src='https___winteriscoming.net_files_image-exchange_2018_08_ie_35830.jpg' alt='The Master (Dhawan) stands in purple victorian dress against a dark background holding some sort of technical device menacingly.'>
+                <img class='images' src='The_Master.jpg' alt='The Master (Dhawan) stands in purple victorian dress against a dark background holding some sort of technical device menacingly.'>
                 <p><button>Add The Master</button></p>";
            echo "<p class='dataBox'>";
        }
@@ -69,7 +69,7 @@ foreach ($forgallifreyArray as $timelord){
                 <video muted loop controls>
                     <source src='video (1).mp4' type='video/mp4'>
                 </video>
-                <img class='images' src='rassilon_the_resurrected_by_timelordghost-da711b2.jpg' alt='Rassilon stands proud against an orange sky background in Red and gold robes holding an imperial staff and wearing a gold collar piece.'>
+                <img class='images' src='Rassilon.jpg' alt='Rassilon stands proud against an orange sky background in Red and gold robes holding an imperial staff and wearing a gold collar piece.'>
                 <p><button>Add Rassilon</button></p>";
            echo "<p class='dataBox'>";
        }
@@ -78,7 +78,7 @@ foreach ($forgallifreyArray as $timelord){
                 <video muted loop controls>
                     <source src='omegavid.mp4' type='video/mp4'>
                 </video>
-                <img class='images' src='Doctor-Who-Omega-appeared-on-Gallifrey-alongside-Tecteun-and-Rassilon-in-the-Matrix-2697746.jpg' alt='A man in dark green robes leans toward the camera against a red background and wearing a large bronze helm.'>
+                <img class='images' src='Omega.jpg' alt='A man in dark green robes leans toward the camera against a red background and wearing a large bronze helm.'>
                 <p><button>Add Omega</button></p>";
            echo "<p class='dataBox'>";
        }
@@ -87,7 +87,7 @@ foreach ($forgallifreyArray as $timelord){
                   <video muted loop controls>
                     <source src='monkvid.mp4' type='video/mp4'>
                 </video>
-                <img class='images' src='2fcax1uo0wv1000.jpg' alt='The Meddling Monk stands on a clifftop with the sun rising behind him and metal
+                <img class='images' src='The_Monk.jpg' alt='The Meddling Monk stands on a clifftop with the sun rising behind him and metal
                 clockwork hanging suspended in midair behind him.'>
                  <p><button>Add The Meddling Monk</button></p>";
            echo "<p class='dataBox'>";
@@ -98,7 +98,7 @@ foreach ($forgallifreyArray as $timelord){
                 <video muted loop controls>
                 <source src='video.mp4' type='video/mp4'>
                 </video>
-                <img class='images' src='images.jpg' alt='A black and white image, Susan looks toward the camera in a leather cap and white collar.'>
+                <img class='images' src='Susan_Foreman.jpg' alt='A black and white image, Susan looks toward the camera in a leather cap and white collar.'>
                 <p><button>Add Susan</button></p>";
            echo "<p class='dataBox'>";
        }
@@ -107,7 +107,7 @@ foreach ($forgallifreyArray as $timelord){
                  <video muted loop controls>
                     <source src='riversongvid.mp4' type='video/mp4'>
                 </video>
-                 <img class='images' src='200px-River_Song_Doctor_Who.png' alt='River Song stands against a multicoloured background in a leather jacket.'>
+                 <img class='images' src='River_Song.png' alt='River Song stands against a multicoloured background in a leather jacket.'>
                  <p><button>Add River Song</button></p>";
            echo "<p class='dataBox'>";
        }
@@ -116,15 +116,15 @@ foreach ($forgallifreyArray as $timelord){
                 <video muted loop controls>
                     <source src='ranivid.mp4' type='video/mp4'>
                 </video>
-                <img class='images' src='220px-The_Rani-Kate_OMara_(1987).jpg' alt='The Rani stands looking imperious in a red and gold tunic with broad sleeves and a technical device on her arm.'>
+                <img class='images' src='The_Rani.jpg' alt='The Rani stands looking imperious in a red and gold tunic with broad sleeves and a technical device on her arm.'>
                 <p><button>Add The Rani</button></p>";
            echo "<p class='dataBox'>";
        }
        if ($aspect=== "id" && $value>8){
            echo "<div class='lord'>";
            echo "<h2>".$name."</h2>";
-           echo "<img class='unknownLord' src='Gallifrey_Sound_of_Drums.jpg' alt='A Time Lord stands in rich flowing red robes, a red skull cap on his head and large golden collar around his neck staring into the distance from atop a bare rock, sky scrapers tower behind him encased in a huge glass dome and red-orange sky burning behind him.'>
-                <img class='stock' src='GallifreyReturns-1-.jpg' alt='A burning red planet against a black background, lit from one side as it orbits its star.'
+           echo "<img class='unknownLord' src='Timelord.jpg' alt='A Time Lord stands in rich flowing red robes, a red skull cap on his head and large golden collar around his neck staring into the distance from atop a bare rock, sky scrapers tower behind him encased in a huge glass dome and red-orange sky burning behind him.'>
+                <img class='stock' src='Gallifrey.jpg' alt='A burning red planet against a black background, lit from one side as it orbits its star.'
            <p></p>";
            echo "<p class='dataBox'>";
        }
@@ -137,7 +137,7 @@ foreach ($forgallifreyArray as $timelord){
 
 <h2>Add Your Own TimeLord</h2>
 
-<form method="post" action="newproject2.php">
+<form method="post" action="index.php">
     <label for="name">Name:</label>
     <input type="text" name="name" id="name" class="formInput"/><br>
 
